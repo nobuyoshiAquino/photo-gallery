9f23f66ffb1f6d23ea1a0e9e599db73fdf578c8b
Nobuyoshi Aquino <nobuyoshi.aquino@gmail.com>
2017-03-24 19:48:06 -0300

    Create a grid list for photos

---
+++ app/src/main/java/xyz/nobu/photogallery/PhotoGalleryActivity.java
@@ -0,0 +1,17 @@
+package xyz.nobu.photogallery;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+
+public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return PhotoGalleryFragment.newInstance();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
