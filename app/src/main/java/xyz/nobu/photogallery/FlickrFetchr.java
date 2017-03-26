b6d741c4a18390a21023e35283fe2566b6efbfee
Nobuyoshi Aquino <nobuyoshi.aquino@gmail.com>
2017-03-25 21:05:40 -0300

    Fetch recent photo captions from Flickr API and load it to the grid list

---
+++ app/src/main/java/xyz/nobu/photogallery/FlickrFetchr.java
@@ -0,0 +1,146 @@
+package xyz.nobu.photogallery;
+
+import android.net.Uri;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by nobu on 3/24/17.
+ */
+
+public class FlickrFetchr {
+
+    private static final String TAG = "FlickrFetchr";
+
+    private static final String API_KEY = "4e4dfa7dc73c2a7f554cf9efe9a32789";
+
+    public byte[] getUrlBytes(String urlSpec) throws IOException {
+
+        /**
+         * Create a URL object from a string - like, say,
+         * https:// www.bignerdranch.com
+         */
+        URL url = new URL(urlSpec);
+
+        /**
+         * openConnection() creates a connection object pointed at
+         * the URL. URL.openConnection() returns a URLConnection,
+         * but because you are connection to an http URL, you can
+         * cast it to HttpURLConnection. This gives you HTTP-specific
+         * interfaces for working with request methods, response codes,
+         * streaming methods, and more.
+         */
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        /**
+         * HttpURLConnection represents a connection, but it will not
+         * actually connect to your endpoint until you call getInputStream()
+         * (or getOutputStrean() for POST calls). Until then, you cannot
+         * get a valid response code.
+         */
+        try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            InputStream in = connection.getInputStream();
+
+            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                throw new IOException(connection.getResponseMessage() +
+                        ": with " +
+                        urlSpec);
+            }
+            /**
+             * Once you create your URL and open a connection, you call
+             * read() repeatedly until your connection runs out of data.
+             * The InputStream will yield bytes as they are available.
+             * When you are done, you close it and spit out your
+             * ByteArrayOutStream's byte array.
+             */
+            int bytesRead = 0;
+            byte[] buffer = new byte[1024];
+            while ((bytesRead = in.read(buffer)) > 0) {
+                out.write(buffer, 0, bytesRead);
+            }
+            out.close();
+            return out.toByteArray();
+        } finally {
+            connection.disconnect();
+        }
+    }
+
+    public String getUrlString(String urlSpec) throws IOException {
+        return new String(getUrlBytes(urlSpec));
+    }
+
+    public List<GalleryItem> fetchItems() {
+
+        List<GalleryItem> items = new ArrayList<>();
+
+        /**
+         * Uri.Builder is a convenience class for creating properly escaped
+         * parameterized URLs.
+         */
+        try {
+            String url = Uri.parse("https://api.flickr.com/services/rest/")
+                    .buildUpon()
+                    .appendQueryParameter("method", "flickr.photos.getRecent")
+                    .appendQueryParameter("api_key", API_KEY)
+                    .appendQueryParameter("format", "json")
+                    .appendQueryParameter("nojsoncallback", "1")
+                    .appendQueryParameter("extras", "url_s")
+                    .build().toString();
+            String jsonString = getUrlString(url);
+            Log.i(TAG, "Received JSON: " + jsonString);
+
+            JSONObject jsonBody = new JSONObject(jsonString);
+            parseItems(items, jsonBody);
+
+        } catch (IOException ioe) {
+            Log.e(TAG, "Failed to fetch items", ioe);
+        } catch (JSONException je) {
+            Log.e(TAG, "Failed to parse JSON", je);
+        }
+
+        return items;
+    }
+
+    private void parseItems(List<GalleryItem> items, JSONObject jsonBody)
+            throws IOException, JSONException {
+
+        /**
+         * Here you have a top-level JSONObject that maps to the outmost
+         * curly braces in the original JSON text. This top-level object
+         * contains a nested JSONObject named photos. Within this nested
+         * JSONObject is a JSONArray named photo. This array contains a
+         * colletion of JSONObjects, each representing metadata for a
+         * single photo.
+         */
+
+        JSONObject photosJsonObject = jsonBody.getJSONObject("photos");
+        JSONArray photoJsonArray = photosJsonObject.getJSONArray("photo");
+
+        for (int i = 0; i < photoJsonArray.length(); i++) {
+            JSONObject photoJsonObject = photoJsonArray.getJSONObject(i);
+
+            GalleryItem item = new GalleryItem();
+            item.setId(photoJsonObject.getString("id"));
+            item.setCaption(photoJsonObject.getString("title"));
+
+            if (!photoJsonObject.has("url_s")) {
+                continue;
+            }
+
+            item.setUrl(photoJsonObject.getString("url_s"));
+            items.add(item);
+        }
+    }
+}
--- app/src/main/java/xyz/nobu/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/xyz/nobu/photogallery/PhotoGalleryFragment.java
@@ -1,5 +1,6 @@
 package xyz.nobu.photogallery;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
@@ -7,6 +8,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by nobu on 3/24/17.
@@ -14,7 +19,10 @@ import android.view.ViewGroup;
 
 public class PhotoGalleryFragment extends Fragment {
 
+    private static final String TAG = "PhotoGalleryFragment";
+
     private RecyclerView mPhotoRecyclerView;
+    private List<GalleryItem> mItems = new ArrayList<>();
 
     public static PhotoGalleryFragment newInstance() {
         return new PhotoGalleryFragment();
@@ -23,7 +31,18 @@ public class PhotoGalleryFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        /**
+         * You retained the fragment so that roation does not repeatedly
+         * fire off new AsyncTasks to fetch the JSON data.
+         */
         setRetainInstance(true);
+
+        /**
+         * The call to execute() will start you AsyncTask, which will
+         * then fire up its background thread and call doInBackground(...)
+         */
+        new FetchItemsTask().execute();
     }
 
     @Override
@@ -34,6 +53,93 @@ public class PhotoGalleryFragment extends Fragment {
         mPhotoRecyclerView = (RecyclerView) v.findViewById(R.id.photo_recycler_view);
         mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
 
+        setupAdapter();
+
         return v;
+    }
+
+    private void setupAdapter() {
+        if (isAdded()) {
+            mPhotoRecyclerView.setAdapter(new PhotoAdapter(mItems));
+        }
+    }
+
+    private class PhotoHolder extends RecyclerView.ViewHolder {
+
+        private TextView mTitleTextView;
+
+        public PhotoHolder(View itemView) {
+            super(itemView);
+
+            mTitleTextView = (TextView) itemView;
+        }
+
+        public void bindGalleryItem(GalleryItem item) {
+            mTitleTextView.setText(item.toString());
+        }
+    }
+
+    private class PhotoAdapter extends RecyclerView.Adapter<PhotoHolder> {
+
+        private List<GalleryItem> mGalleryItems;
+
+        public PhotoAdapter(List<GalleryItem> galleryItems) {
+            mGalleryItems = galleryItems;
+        }
+
+        @Override
+        public PhotoHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            TextView textView = new TextView(getActivity());
+            return new PhotoHolder(textView);
+        }
+
+        @Override
+        public void onBindViewHolder(PhotoHolder photoHolder, int position) {
+            GalleryItem galleryItem = mGalleryItems.get(position);
+            photoHolder.bindGalleryItem(galleryItem);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mGalleryItems.size();
+        }
+    }
+
+    /**
+     * The easiest way to work with a background thread is with a utility
+     * class called AsyncTask, which creates a background thread for you
+     * and runs the code in the doInBackground(...) method on the thread.
+     *
+     * The first type parameter allows you to specify the type of input
+     * parameters you will pass to execute(), which in turn dictates the
+     * type of input parameters doInBackground(...) will receive.
+     *
+     * The second type parameter allows you to specify the type for
+     * sending progress updates.
+     * Progress updates usually happen in the middle of an ongoing background
+     * process. The problem is that you cannot make the necessary UI updates
+     * inside the background process. So AsyncTask provides publishProgress(...)
+     * and onProgressUpdate(...).
+     * You call publishProgress(...) from doInBackground(...) in the back-
+     * -ground thread. This will make onProgressUpdate(...) be called on the
+     * UI thread. So you can do your UI updates in onProgressUpdate(...),
+     * but control them from doInBackground(...) with publishProgress(...).
+     *
+     * The third parameter is the type of result produced by your AsyncTask.
+     * It sets the type of value returned by doInBackground(...) as well
+     * as the type of onPostExecute(...)'s input parameter.
+     */
+    private class FetchItemsTask extends AsyncTask<Void, Void, List<GalleryItem>> {
+
+        @Override
+        protected List<GalleryItem> doInBackground(Void... params) {
+            return new FlickrFetchr().fetchItems();
+        }
+
+        @Override
+        protected void onPostExecute(List<GalleryItem> items) {
+            mItems = items;
+            setupAdapter();
+        }
     }
 }
