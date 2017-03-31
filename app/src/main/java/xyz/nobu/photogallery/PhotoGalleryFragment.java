84635c6417f24e80f869dae0957a5beff6ce5614
Nobuyoshi Aquino <nobuyoshi.aquino@gmail.com>
2017-03-30 23:11:03 -0400

    Add search action view to submit queries

--- app/src/main/java/xyz/nobu/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/xyz/nobu/photogallery/PhotoGalleryFragment.java
@@ -9,8 +9,12 @@ import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -43,12 +47,13 @@ public class PhotoGalleryFragment extends Fragment {
          * fire off new AsyncTasks to fetch the JSON data.
          */
         setRetainInstance(true);
+        setHasOptionsMenu(true);
 
         /**
          * The call to execute() will start you AsyncTask, which will
          * then fire up its background thread and call doInBackground(...)
          */
-        new FetchItemsTask().execute();
+        updateItems();
 
         /**
          * Remember that by default, the Handler will attach itself to the
@@ -104,6 +109,56 @@ public class PhotoGalleryFragment extends Fragment {
         super.onDestroy();
         mThumbnailDownloader.quit();
         Log.i(TAG, "Background thread destroyed");
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater menuInflater) {
+        super.onCreateOptionsMenu(menu, menuInflater);
+        menuInflater.inflate(R.menu.fragment_photo_gallery, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.menu_item_search);
+        final SearchView searchView = (SearchView) searchItem.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                Log.d(TAG, "QueryTextSubmit: " + s);
+                QueryPreferences.setStoredQuery(getActivity(), s);
+                updateItems();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                Log.d(TAG, "QueryTextChange: " + s);
+                return false;
+            }
+        });
+
+        searchView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String query = QueryPreferences.getStoredQuery(getActivity());
+                searchView.setQuery(query, false);
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_clear:
+                QueryPreferences.setStoredQuery(getActivity(), null);
+                updateItems();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void updateItems() {
+        String query = QueryPreferences.getStoredQuery(getActivity());
+        new FetchItemsTask(query).execute();
     }
 
     private void setupAdapter() {
@@ -181,16 +236,19 @@ public class PhotoGalleryFragment extends Fragment {
      * as the type of onPostExecute(...)'s input parameter.
      */
     private class FetchItemsTask extends AsyncTask<Void, Void, List<GalleryItem>> {
+        private String mQuery;
+
+        public FetchItemsTask(String query) {
+            mQuery = query;
+        }
 
         @Override
         protected List<GalleryItem> doInBackground(Void... params) {
 
-            String query = "robot"; // Just for testing
-
-            if (query == null) {
+            if (mQuery == null) {
                 return new FlickrFetchr().fetchRecentPhotos();
             } else {
-                return new FlickrFetchr().searchPhotos(query);
+                return new FlickrFetchr().searchPhotos(mQuery);
             }
         }
 
---
+++ app/src/main/java/xyz/nobu/photogallery/QueryPreferences.java
@@ -0,0 +1,24 @@
+package xyz.nobu.photogallery;
+
+import android.content.Context;
+import android.preference.PreferenceManager;
+
+/**
+ * Created by nobu on 3/30/17.
+ */
+
+public class QueryPreferences {
+    private static final String PREF_SEARCH_QUERY = "searchQuery";
+
+    public static String getStoredQuery(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getString(PREF_SEARCH_QUERY, null);
+    }
+
+    public static void setStoredQuery(Context context, String query) {
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putString(PREF_SEARCH_QUERY, query)
+                .apply();
+    }
+}
