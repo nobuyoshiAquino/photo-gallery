12fca612e7de7c12c0aeab654e94a140b4d6e2d5
Nobuyoshi Aquino <nobuyoshi.aquino@gmail.com>
2017-04-05 22:59:08 -0400

    Restore the poll service on device's boot

---
+++ app/src/main/java/xyz/nobu/photogallery/StartupReceiver.java
@@ -0,0 +1,23 @@
+package xyz.nobu.photogallery;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by nobu on 4/5/17.
+ */
+
+public class StartupReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "StartupReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i(TAG, "Received broadcast intent: " + intent.getAction());
+
+        boolean isOn = QueryPreferences.isAlarmOn(context);
+        PollService.setServiceAlarm(context, isOn);
+    }
+}
